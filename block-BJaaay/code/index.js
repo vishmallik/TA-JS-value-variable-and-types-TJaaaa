f9a514c06e828948d62d3d971ee4d11ae9b766b2
vishmallik <vishmallik@gmail.com>
2022-06-28 18:06:48 +0530

    add loops assignment

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,11 +3,33 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
+for (let i=1; i<11; i++){
+    console.log(i);
+}
 
 // Using `console.log` log all the evan values from 1 to 10.
+for (let i=1; i<11; i++){
+    if(i%2===0){
+        console.log(i);
+    }
+}
 
 // Using `console.log` log all the od values from 1 to 10.
-
+for (let i=1; i<11; i++){
+    if(i%2!==0){
+        console.log(i);
+    }
+}
 // Calculate the sum of all numbers from 1 to 10.
+let sum=0;
+for (let i=1; i<11; i++){
+    sum=sum+i;
+}
+console.log(sum);
 
 // Log all the values from 1 to 10 using while loop
+let i=1;
+while(i<11){
+    console.log(i);
+    i++;
+}
